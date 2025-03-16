68451ddb4d61349f296733174bc338423035998a
attilajam <attila.jamilov@gmail.com>
2025-03-16 13:57:20 -0700

    lol

--- get_prompt.py
+++ get_prompt.py
@@ -1,7 +1,6 @@
 import os 
 from dotenv import load_dotenv
-import google.generativeai as genai
-
+from google import genai
 load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY)
